63397e783990b70bf767fed7e92ca0328b6c6c61
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-04-29 19:16:50 +0200

    Split logic into sendNotification and sendNotifications methods

--- src/Server/ClientHandler.java
+++ src/Server/ClientHandler.java
@@ -7,6 +7,7 @@ import java.io.PrintWriter;
 import java.net.SocketException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -15,33 +16,36 @@ import Shared.Notification;
 
 public class ClientHandler implements Runnable {
     private final Client client;
+    private PrintWriter out;
+    private BufferedReader in;
 
     public ClientHandler(Client client) {
         this.client = client;
+        try {
+            out = new PrintWriter(client.getSocket().getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(client.getSocket().getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void run() {
         try {
-            final PrintWriter out = new PrintWriter(client.getSocket().getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(client.getSocket().getInputStream()));
-
             String line;
             while ((line = in.readLine()) != null) {
-                // line - json string
+                if (line.equals("history")) {
+                    sendNotifications();
+                    continue;
+                }
+
+                if(line.equals("exit")) {
+                    break;
+                }
+                
                 Notification notification = Notification.fromJson(line);
                 client.addNotification(notification);
-
-                Timer timer = new Timer();
-                timer.schedule(new TimerTask() {
-                    @Override
-                    public void run() {
-                        notification.setDateTimeAtCompleted(
-                                LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
-                        System.out.println("Sending message: " + notification.getMessage());
-                        out.println(notification.toJson());
-                    }
-                }, notification.getDelayInSeconds() * 1000);
+                sendNotification(notification);
             }
         } catch (SocketException e) {
             System.out.println("Client disconnected");
@@ -49,11 +53,37 @@ public class ClientHandler implements Runnable {
             e.printStackTrace();
         } finally {
             try {
-                client.getSocket().close();
+                if (out != null) {
+                    out.close();
+                }
+                if (in != null) {
+                    in.close();
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    public void sendNotification(Notification notification) {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                notification.setDateTimeAtCompleted(
+                        LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+                System.out.println("Sending message: " + notification.getMessage());
+                out.println(notification.toJson());
+            }
+        }, notification.getDelayInSeconds() * 1000);
+    }
+
+    public void sendNotifications() {
+        List<Notification> notifications = client.getNotifications();
+        for (Notification notification : notifications) {
+            out.println(notification.toJson());
+        }
+        out.println("end");
     }
 
 }
