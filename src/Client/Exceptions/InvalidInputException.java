d3e08dd3c0332147737cdad8cd636ac298d56cbd
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-05-09 14:23:25 +0200

    Remove unnecessary constructors

--- src/Client/Exceptions/InvalidInputException.java
+++ src/Client/Exceptions/InvalidInputException.java
@@ -4,13 +4,4 @@ public class InvalidInputException extends Exception {
     public InvalidInputException(String s) {
         super(s);
     }
-    public InvalidInputException() {
-    }
-
-    public String InvalidInputException(String message) {
-        return message;
-    }
-    public String InvalidInputException() {
-        return "Invalid input";
-    }
 }
