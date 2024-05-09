f5decfbef3ac1fb7b2090b2ff1ae4af3f20cdb29
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-05-09 14:22:31 +0200

    Add custom exceptions

---
+++ src/Client/Exceptions/EmptyMessageException.java
@@ -0,0 +1,7 @@
+package Client.Exceptions;
+
+public class EmptyMessageException extends Exception{
+    public EmptyMessageException(String message) {
+        super(message);
+    }
+}
---
+++ src/Client/Exceptions/InvalidDurationException.java
@@ -0,0 +1,7 @@
+package Client.Exceptions;
+
+public class InvalidDurationException extends Exception {
+    public InvalidDurationException(String message) {
+        super(message);
+    }
+}
